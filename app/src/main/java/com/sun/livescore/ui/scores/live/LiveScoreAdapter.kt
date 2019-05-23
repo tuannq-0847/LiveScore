594a4f1625eb28d1541733d269861a3896b09171
tuannq-0847 <nguyen.quoc.tuan@framgia.com>
2019-05-23 09:53:07 +0700

    fix comment

--- app/src/main/java/com/sun/livescore/ui/scores/live/LiveScoreAdapter.kt
+++ app/src/main/java/com/sun/livescore/ui/scores/live/LiveScoreAdapter.kt
@@ -12,6 +12,7 @@ import com.sun.livescore.ui.base.BaseViewHolder
 import com.sun.livescore.ui.scores.child.ScoreHistoryAdapter.HistoryViewHolder
 import com.sun.livescore.util.Constant
 import com.sun.livescore.util.Util
+import kotlinx.android.synthetic.main.item_match_score.view.textNameLeague
 import kotlinx.android.synthetic.main.item_match_score.view.viewOnline
 
 class LiveScoreAdapter(private val histories: List<History>) :
@@ -35,23 +36,30 @@ class LiveScoreAdapter(private val histories: List<History>) :
 
         override fun bindView(binding: ItemMatchScoreBinding, position: Int, data: History) {
             binding.run {
-                when {
-                    position + 1 < histories.size && (histories[position].leagueName == histories[position + 1].leagueName) -> textNameLeague.visibility =
-                        View.GONE
-                }
-
-                itemView.viewOnline.visibility = View.VISIBLE
                 isScore = false
-                timeHistory = if (data.time != HALF_TIME) {
-                    "${data.time}'"
-                } else {
-                    data.time
-                }
+                checkSameLeague(position, itemView)
+                itemView.viewOnline.visibility = View.VISIBLE
+                showValidTime(binding, data)
                 scoreHistory = data
                 val scores = Util.getScoreFromString(data.score)
                 scoreHomeTeam = scores[Constant.FIRST_SCORE_INDEX]
                 scoreAwayTeam = scores[Constant.SECOND_SCORE_INDEX]
             }
+        }
+    }
+
+    private fun showValidTime(binding: ItemMatchScoreBinding, data: History) {
+        binding.timeHistory = if (data.time != HALF_TIME) {
+            "${data.time}'"
+        } else {
+            data.time
+        }
+    }
+
+    private fun checkSameLeague(position: Int, itemView: View) {
+        when {
+            position + 1 < histories.size && (histories[position].leagueName == histories[position + 1].leagueName)
+            -> itemView.textNameLeague.visibility = View.GONE
         }
     }
 
